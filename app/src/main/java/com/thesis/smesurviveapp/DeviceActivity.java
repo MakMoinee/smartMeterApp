97e00e558415ff7a2cc1575adc87bb5bf720e94a
Titus Silver <titus.silver.ph@gmail.com>
2025-05-16 13:49:06 +0800

    update code

--- app/src/main/java/com/thesis/smesurviveapp/DeviceActivity.java
+++ app/src/main/java/com/thesis/smesurviveapp/DeviceActivity.java
@@ -49,7 +49,7 @@ public class DeviceActivity extends AppCompatActivity {
         @Override
         public void run() {
             loadData();
-            handler.postDelayed(this, 5000); // run again after 30 seconds
+            handler.postDelayed(this, 3000); // run again after 30 seconds
         }
     };
 
